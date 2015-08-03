090a92bbb56210cd30aefb3e5614fb0540ce52b7
aaa <8>
2015-08-03 12:03:22 +0800

    babytest

--- js/func.js
+++ js/func.js
@@ -1,3 +1,11 @@
 /**
  * Created by adwo15513 on 2015/7/31.
  */
+function resetPage() {
+    var deviceWidth = document.documentElement.clientWidth,
+        scale = deviceWidth / 320;
+    document.body.style.zoom = scale;
+}
+function $(s){
+    return document.querySelector(s);
+}
--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,36 @@
 /**
  * Created by adwo15513 on 2015/7/31.
  */
+window.onload = function() {
+    $('.ping').addEventListener('webkitAnimationStart',function(){
+        $('.t1').style.display='block';
+        setTimeout(function(){
+            $('.t2').style.display='block';
+            setTimeout(function(){
+                $('.t3').style.display='block';
+                setTimeout(function(){
+                    $('.t4').style.display='block';
+                },200)
+            },200)
+        },500)
+    })
+    resetPage();
+    $('.ping').addEventListener('webkitAnimationEnd',function(){
+        $('.y1').style.opacity=1;
+        setTimeout(function(){
+            $('.y1').style.opacity=0;
+            setTimeout(function(){
+                $('.y2').style.opacity=1;
+                setTimeout(function(){
+                    $('.y2').style.opacity=0;
+                    $('.y3').style.webkitTransform='scale(1,1)';
+                },200)
+            },200)
+        },200)
+    })
+
+
+}
+window.onresize = function() {
+    resetPage();
+}
