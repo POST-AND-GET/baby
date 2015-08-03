b5bf9920d643522269a98d0ba7229f9e3ce27105
aaa <8>
2015-08-03 18:50:55 +0800

    baby

--- js/func.js
+++ js/func.js
@@ -6,6 +6,114 @@ function resetPage() {
         scale = deviceWidth / 320;
     document.body.style.zoom = scale;
 }
-function $(s){
+function $$(s){
     return document.querySelector(s);
+}
+var ctx, cvs, ctx1, cvs1,strDataURI1,resultid;
+function canvasInit(){
+    cvs = $$('#cvs');
+    cvs.width = 200;
+    cvs.height = 144;
+    ctx = cvs.getContext('2d');
+    ctx.clearRect(-98,-75,400,244);
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(11,17,175,177);
+}
+function canvasInit1(){
+    cvs1 = $$('#canvas');
+    cvs1.width = 600;
+    cvs1.height = 400;
+    ctx1 = cvs1.getContext('2d');
+    ctx1.clearRect(-300,-200,1200,800);
+    ctx1.fillStyle = '#ffffff';
+    ctx1.fillRect(0,0,600,400);
+}
+
+function readFile(){
+    var file = this.files[0];
+    if(!/image\/\w+/.test(file.type)){
+        alert("请确保文件为图像类型");
+        return false;
+    }
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function(e){
+        var img = new Image;
+        img.src=this.result;
+        img.onload = function(){
+
+            ctx.drawImage(img,11,17,175,117);
+            ctx1.drawImage(img,0,0,600,400);
+
+            $$('.go-index').style.display = 'none';
+            $$('.reset').style.display = 'block';
+            $$('.up').style.display = 'none';
+            $$('.sub').style.display = 'block';
+
+            strDataURI1 = cvs1.toDataURL();
+            ctx.translate(98,75);
+            ctx1.translate(300,200);
+            $$('.evert').addEventListener('touchstart',function(){
+                ctx.clearRect(-98,-75,400,244);
+                ctx.fillRect(-87,-58,175,177);
+                ctx.rotate(-90 * Math.PI / 180);
+                ctx.drawImage(img,-87,-58,175,117);
+                ctx1.clearRect(-300,-200,600,400);
+                ctx1.fillRect(-300,-200,600,400);
+                ctx1.rotate(-90 * Math.PI / 180);
+                ctx1.drawImage(img,-300,-200,600,400);
+                strDataURI1 = cvs1.toDataURL();
+                console.log(strDataURI1)
+            });
+
+        }
+    }
+}
+function doInput(id){
+    var inputObj = document.createElement('input');
+    inputObj.addEventListener('change',readFile,false);
+    inputObj.type = 'file';
+    inputObj.accept = 'image/*';
+    inputObj.id = id;
+    inputObj.click();
+}
+function isWord(w) {
+    var partten = /^[\u4E00-\u9FFF]+$$/;
+    if (partten.test(w)) {
+        return true
+    } else {
+        alert('请正确输入名字！！');
+        return false
+    }
+}
+function isTelNum(num) {
+    var partten = /^1[3,5,7,8]\d{9}$$/;
+    if (partten.test(num)) {
+        return true
+    } else {
+        alert('请正确输入手机号码！！');
+        return false
+    }
+}
+function callbackInfo(data){
+    console.log(data)
+    if(data[0].success==1){
+        resultid = data[0].id;
+        console.log(resultid)
+        $$('.form').style.display = 'none';
+        $$('.upload').style.display = 'block';
+    }
+    if(data[0].success==3){
+        alert(data[0].info);
+    }
+}
+function callbackImg(data){
+    console.log(data)
+}
+function Ajax(url){
+    var script = document.createElement('script');
+    script.setAttribute("type","text/javascript");
+    script.src = url;
+    document.body.appendChild(script);
+    //script.remove();
 }
--- js/logic.js
+++ js/logic.js
@@ -1,39 +1,99 @@
-/**
- * Created by adwo15513 on 2015/7/31.
- */
 window.onload = function() {
-
     resetPage();
-    $('.ping').addEventListener('webkitAnimationStart',function(){
-        $('.t1').style.display='block';
-        setTimeout(function(){
-            $('.t2').style.display='block';
-            setTimeout(function(){
-                $('.t3').style.display='block';
-                setTimeout(function(){
-                    $('.t4').style.display='block';
-                },200)
-            },200)
-        },500)
+    $$('.ping').addEventListener('webkitAnimationStart', function() {
+        $$('.t1').style.display = 'block';
+        setTimeout(function() {
+            $$('.t2').style.display = 'block';
+            setTimeout(function() {
+                $$('.t3').style.display = 'block';
+                setTimeout(function() {
+                    $$('.t4').style.display = 'block'
+                }, 200)
+            }, 200)
+        }, 500)
     });
-    $('.ping').addEventListener('webkitAnimationEnd',function(){
-        $('.y1').style.opacity=1;
-        setTimeout(function(){
-            $('.y1').style.opacity=0;
-            setTimeout(function(){
-                $('.y2').style.opacity=1;
-                setTimeout(function(){
-                    $('.y2').style.opacity=0;
-                    $('.y3').style.webkitTransform='scale(1,1)';
-                },200)
-            },200)
-        },200)
+    $$('.ping').addEventListener('webkitAnimationEnd', function() {
+        $$('.y1').style.opacity = 1;
+        setTimeout(function() {
+            $$('.y1').style.opacity = 0;
+            setTimeout(function() {
+                $$('.y2').style.opacity = 1;
+                setTimeout(function() {
+                    $$('.y2').style.opacity = 0;
+                    $$('.y3').style.webkitTransform = 'scale(1,1)'
+                }, 200)
+            }, 200)
+        }, 200)
     });
-    $('.pai').addEventListener('touchstart',function(){
-        alert(1)
-    })
+    $$('.pai').addEventListener('touchstart', function(e) {
+        $$('.form').style.display = 'block';
+        $$('.form .close').onclick = function() {
+            $$('.form').style.display = 'none'
+        }
+    }); $$('.guide').addEventListener('touchstart', function() {
+        $$('.form').style.display = 'none';
+        $$('.tips').style.display = 'block';
+        $$('.tips .close').onclick = function() {
+            $$('.tips').style.display = 'none'
+        }
+    });
+    $$('.go-index').addEventListener('touchstart', function() {
+        location.href = 'index.html'
+    });
+    $$('.up').addEventListener('touchstart', function() {
+        doInput('inputObj')
+    });
+    $$('.reset').addEventListener('touchstart', function() {
+        canvasInit();
+        doInput('inputObj1')
+    });
 
+    canvasInit();
+    canvasInit1() ;
+    $$('.submit').addEventListener('touchstart', function() {
+        var name = $$('.name').value;
+        var mobile = $$('.mobile').value;
+        if (isWord(name) && isTelNum(mobile)) {
+            var _u = "http://test.zhangkuo.net/advmessage/adv/addResultJsonP.action?advid=30380&realname=" + name + "&mobile=" + mobile+"&callback=callbackInfo";
+            Ajax(_u);
+        }
+    });
+    $$('.sub').addEventListener('touchstart', function() {
+        var _u = "http://test.zhangkuo.net/advmessage/advimage/saveImgJsonP.action?advid=30380&resultid=" + resultid;
+        //var xmlHttp;
+        //try { xmlHttp = new XMLHttpRequest(); }
+        //catch( e ){
+        //    try { xmlHttp = new ActiveXObject( "Msxml2.XMLHTTP" );}
+        //    catch ( e ){
+        //        try { xmlHttp = new ActiveXObject( "Microsoft.XMLHTTP" );}
+        //        catch( e ) { alert("您的浏览器不支持AJAX！");return false;}
+        //    }
+        //}
+        //xmlHttp.open('POST',_u,true);
+        //xmlHttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        //xmlHttp.onreadystatechange = function () {
+        //    if (xmlHttp.readyState == 4) {
+        //        if (xmlHttp.status == 200) {
+        //           console.log(xmlHttp.responseText);
+        //        } else {
+        //            console.log(xmlHttp.responseText);
+        //        }
+        //    }
+        //}
+        $.ajax({
+            url: _u,
+            type: 'POST',
+            data:{ imagebase64: strDataURI1 },
+            cache: false,
+            timeout: 60000,
+            dataType:'json',
+            success: function(d){
+            console.log(d)
+        }});
+
+        //Ajax(_u);
+    });
 }
 window.onresize = function() {
-    resetPage();
+    resetPage()
 }
